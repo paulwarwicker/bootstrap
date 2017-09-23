15c134336d59c669c2c4de52203c90cbb07215a0
paulwarwicker <paul.warwicker@gmail.com>
2017-09-23 23:11:42 +0100

    initial commit

---
+++ boxstart_adv.ps1
@@ -0,0 +1,5 @@
+. { iwr -useb http://boxstarter.org/bootstrapper.ps1 } | iex; get-boxstarter -Force
+Install-BoxstarterPackage -PackageName https://raw.githubusercontent.com/paulwarwicker/bootstrap/master/bootstrap_adv.ps1 -DisableReboots
+
+#$cred=Get-Credential destiny\paulw
+#Install-BoxstarterPackage -PackageName https://raw.githubusercontent.com/paulwarwicker/bootstrap/master/bootstrap_adv.ps1 -Credential $cred
